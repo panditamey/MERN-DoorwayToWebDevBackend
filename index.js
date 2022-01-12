527f8cf94fd458a0ab10eb961ac94fd7bb949921
unknown <codewithamey@gmail.com>
2022-01-12 17:55:20 +0530

    server pushed

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://amey:<amey>@amey.quiud.mongodb.net/d1?retryWrites=true&w=majority"
+);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("BOOM SERVER RUNS 🔥🔥🔥🔥");
+});
